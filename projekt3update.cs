00d18130e013eb88932e6a29c009849af62d1b65
Samuel <113968434+flithysn@users.noreply.github.com>
2023-04-28 22:41:33 +0200

    Čtvrtá verze školního projektu
    + Náhodný výběr slov nyní i u třetí možnosti
    + odchylka u výsledku změněna na 0.1
    + změněno rozmení generace náhodných čísel

--- projekt3update.cs
+++ projekt3update.cs
@@ -1,48 +1,62 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         Random random = new Random();
+        bool repeat = true;
 
-        while (true)
+        while (repeat)
         {
             int option = random.Next(1, 4);
-            double value1 = Math.Round(random.NextDouble() * 30 + 60, 1);
-            double value2 = Math.Round(random.NextDouble() * 90 + 10, 1);
-            double value3 = Math.Round(random.NextDouble() * 10 + 40, 1);
+            double value1 = Math.Round(random.NextDouble() * 20 + 160, 1);
+            double value2 = Math.Round(random.NextDouble() * 26 + 83, 1);
+            double value3 = Math.Round(random.NextDouble() * 18 + 213, 1);
 
-            string[] Domacnost = { "Válec v pračce", "", "Gramofonní deska", "Odšťavňovač", "Vystavený talíř", "Kamera 3000", "Vinylová deska", "ručička na hodinách" };
+            string[] Domacnost = { "Válec v pračce", "Gramofonní deska", "Odšťavňovač", "Vystavený talíř", "Google Maps Kamera 3000", "Vinylová deska", "Ručička na hodinách" };
+
+            Random rmd = new Random();
+            int cislo = rmd.Next(Domacnost.Length);
+            string Domaci = Domacnost[cislo];
+
             string[] dopravniProstredek = { "Zadní kolo auta", "Zašpiněné kolo vlaku", "Přední vrtule letadla", "Kapitánův radar na loďi", "Přední kolo motorky", "Malé kolo dětského scooteru", "Rotovací mechanismus bicyklu", "Velká vrtule helikoptéry" };
 
-            string randomObject = option == 1 ? Domacnost[random.Next(Domacnost.Length)] : dopravniProstredek[random.Next(dopravniProstredek.Length)];
-            double answer = 0;
+            Random rnd = new Random();
+            int index = rnd.Next(dopravniProstredek.Length);
+            string nahodnyDopravniProstredek = dopravniProstredek[index];
+
+            string[] nahodnost = { "střed turbíny", "Čerstvé umyté kolo vlaku", "Záložná vrtule helikoptéry, která se náhle začala otáčet, ", "Malá ručička na dětských hodinkách", "DVD Harryho Pottera", "CD" };
+
+            Random rdm = new Random();
+            int nahoda = rdm.Next(nahodnost.Length);
+            string nahodnakulatost = nahodnost[nahoda];
 
             switch (option)
             {
                 case 1:
-                    Console.Write($"{randomObject} se otáčí obvodovou rychlostí {value2}cm/s s poloměrem o velikosti {value1}cm. Vypočtěte jeho úhlovou rychlost. ");
-                    answer = value1 / value2;
+                    Console.Write($"{Domaci} se otáčí obvodovou rychlostí {value2}cm/s s poloměrem o velikosti {value1}cm. Vypočtěte jeho úhlovou rychlost. ");
+                    double answer1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(Math.Abs(answer1 - value1 / value2) < 0.1 ? "Správná odpověď!" : $"Špatná odpověď :( ({ Math.Round(value1 / value2, 1)} je správny výsledek, používáme vzorec:ω = v/r)");
                     break;
 
                 case 2:
-                    Console.Write($"{randomObject} se otáčí úhlovou rychlostí o velikosti {value3} rad/s a obvodovou rychlostí o velikosti {value2} cm/s. Vypočtěte poloměr tohoto kola. ");
-                    answer = value2 / value3;
+                    Console.Write($"{nahodnyDopravniProstredek} se otáčí úhlovou rychlostí o velikosti {value3} rad/s a obvodovou rychlostí o velikosti {value2} cm/s. Vypočtěte poloměr tohoto kola. ");
+                    double answer2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(Math.Abs(answer2 - value2 / value3) < 0.1 ? "Správná odpověď!" : $"Špatná odpověď :( ({Math.Round(value2 / value3, 1)} je správny výsledek, používáme vzorec:r = v/ω)");
                     break;
 
                 case 3:
                     Console.Write($"Gramofonová deska s poloměrem o velikosti {value1}cm se otáčí s úhlovou rychlostí {value3}rad/s. Vypočtěte obvodovou rychlost gramofonní desky. ");
-                    answer = value1 * value3;
+                    double answer3 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(Math.Abs(answer3 - value1 * value2) < 0.1 ? "Správná odpověď!" : $"Špatná odpověď :( ({Math.Round(value1 * value3, 1)} je správný výsledek, používáme vzorec:v = r * ω)");
                     break;
             }
 
-            double userInput = double.Parse(Console.ReadLine());
-            Console.WriteLine(Math.Abs(userInput - answer) < 1 ? "Správná odpověď!" : $"Špatná odpověď :( ({answer} je správný výsledek)");
-
             Console.WriteLine("Chcete vytvořit další příklad? y = ano; n = ne");
-            if (Console.ReadLine() != "y")
-                break;
+            string input = Console.ReadLine();
+            if (input != "y")
+                repeat = false;
         }
     }
 }
